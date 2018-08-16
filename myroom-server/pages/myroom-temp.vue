704170d591d48d7f33aece848f8462fc47e6da92
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-08-16 18:41:12 +0900

    部屋の温度表示アプリ追加

---
+++ myroom-server/pages/myroom-temp.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        roooooooom
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+    </div>
+    <p>{{sensorData}}</p>
+  </section>
+</template>
+
+<script>
+  import firebase from 'firebase'
+  export default {
+    async data () {
+      const db = firebase.database();
+      const ref = db.ref("temp-humid-sensor");
+      const snapshot = await ref.once('value');
+      return { sensorData: snapshot.val() };
+    },
+    mounted () {
+      const config = require("../assets/config.json");
+      firebase.initializeApp(config);
+
+    }
+  }
+
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
