b7b9925fdd8ff60e697ccbd8ed676710c99639ba
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-08-21 22:41:13 +0900

    グラフのリアルタイム更新成功

--- myroom-server/pages/myroom-temp.vue
+++ myroom-server/pages/myroom-temp.vue
@@ -1,57 +1,18 @@
 <template>
   <div>
-    <line-chart v-if="showLine" :data="chartData" :options="options"/>
+    <line-chart v-if="showLine"/>
   </div>
 </template>
 
 <script>
-  import firebase from 'firebase'
-
-  const ParseSensorData2ChartData = function (sensorData) {
-    const x = []
-    const y = []
-    for (let key in sensorData) {
-      // HACK:時系列で入ってくる確証はない？ちゃんと降順ソートすべき
-      x.unshift(sensorData[key]["timestamp"]);
-      y.unshift(sensorData[key]["tmp"])
-    }
-    const chartData = {
-      labels: x,
-      datasets: [
-        {
-          label: 'temperature',
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: y,
-        }
-      ]
-    };
-    return chartData;
-  }
-
   export default {
     data () {
       return {
-        showLine: false,
-        chartData: {},
-        options: {}
+        showLine: false
       };
     },
     mounted () {
       this.showLine = true // showLine will only be set to true on the client. This keeps the DOM-tree in sync.
-
-      const config = require("../assets/config.json");
-      firebase.initializeApp(config);
-      const db = firebase.database();
-      const ref = db.ref("temp-humid-sensor").limitToLast(100);
-      ref.on('value', ( snapshot ) => {
-        this.chartData = ParseSensorData2ChartData(snapshot.val());
-        console.log(this.chartData)
-      })
     }
   }
 
--- myroom-server/plugins/vue-chartjs.js
+++ myroom-server/plugins/vue-chartjs.js
@@ -1,10 +1,51 @@
 import Vue from 'vue'
 import { Line } from 'vue-chartjs'
+import firebase from 'firebase'
+
+const ParseSensorData2ChartData = function (sensorData) {
+  const x = []
+  const y = []
+  for (let key in sensorData) {
+    // HACK:時系列で入ってくる確証はない？ちゃんと降順ソートすべき
+    x.push(sensorData[key]["timestamp"]);
+    y.push(Number(sensorData[key]["tmp"]));
+  }
+  const chartData = {
+    labels: x,
+    datasets: [
+      {
+        label: 'temperature',
+        data: y,
+      }
+    ]
+  };
+  return chartData;
+}
 
 Vue.component('line-chart', {
   extends: Line,
-  props: ['data', 'options'],
-  mounted() {
-    this.renderChart(this.data, this.options);
+  mounted () {
+    const options = {
+      elements: {
+        line: {
+          tension: 0,
+          fill: false,
+        }
+      },
+      animation: { duration: 0 },
+      hover: { animationDuration: 0 },
+      responsiveAnimationDuration: 0,
+      maintainAspectRatio: false
+    }
+
+    const config = require("../assets/config.json");
+    firebase.initializeApp(config);
+    const db = firebase.database();
+    const ref = db.ref("temp-humid-sensor").limitToLast(100);
+
+    ref.on('value', ( snapshot ) => {
+      const chartData = ParseSensorData2ChartData(snapshot.val());
+      this.renderChart(chartData, options);
+    })
   }
 });
