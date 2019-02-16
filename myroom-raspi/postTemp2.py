e2b3fb0b1c1fed2b3284e95bd2335f81a8661bf8
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-02-16 21:49:22 +0900

    create postTemp2.py to put data on firestore

---
+++ myroom-raspi/postTemp2.py
@@ -0,0 +1,26 @@
+import dht11
+import datetime
+import RPi.GPIO as GPIO
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Get sensor data
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+mydht11 = dht11.DHT11(pin=14)
+dht11_read_data = mydht11.read()
+
+# Add data on firestore
+cred = credentials.Certificate('/home/pi/nuxtfire/myroom-8924e-firebase-adminsdk-q189k-2a864458d0.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+doc_ref = db.collection(u'temp-humid-sensor')
+doc_ref.add({
+    u'timestamp': datetime.datetime.now(),
+    u'tmp': dht11_read_data.temperature,
+    u'hmd': dht11_read_data.humidity
+    })
