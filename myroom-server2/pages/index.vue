f34e4478a63f839cf9bb5676440b11e600474c3f
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-02-19 23:31:03 +0900

    show temperature graph

--- myroom-server2/pages/index.vue
+++ myroom-server2/pages/index.vue
@@ -1,10 +1,29 @@
 <template lang="pug">
   div
-   h1 hello world!
+   h1 室温グラフ
+   v-sparkline(
+      :value="tmps"
+      :labels="timestamps"
+      line-width="1"
+      label-size="1"
+    )
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
+  computed: {
+    ...mapState('tempHumidSensor', ['tempHumidSensor']),
+    tmps() {
+      return this.tempHumidSensor.map(x => x.tmp)
+    },
+    hmds() {
+      return this.tempHumidSensor.map(x => x.hmd)
+    },
+    timestamps() {
+      return this.tempHumidSensor.map(x => x.timestamp)
+    },
+  },
   mounted() {
     this.$store.dispatch('tempHumidSensor/startListener')
   },
